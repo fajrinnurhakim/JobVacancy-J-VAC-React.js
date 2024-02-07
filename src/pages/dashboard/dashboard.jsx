45b89e7bb93f537a86adf711c2fd6b41521bdc08
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-02-07 23:47:56 +0700

    change

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -7,17 +7,17 @@ export default function Dashboard() {
                 <meta charSet="utf-8" />
                 <title>Dashboard | J-VAC</title>
             </Helmet>
-            <div className="flex flex-col xs-2 py-5 lg:px-12 lg:flex-row">
+            <div className="flex flex-col py-5 xs-2 lg:px-12 lg:flex-row">
                 <div className="w-8/12 mx-auto md:w-1/2 lg:w-1/2">
                     <img src="/assets/Image-hero2.svg" alt="hero image" />
                 </div>
 
-                <div className="w-full px-2 text-center space-y-5 flex flex-col justify-center items-center lg:w-1/2 lg:pe-24 lg:text-left">
+                <div className="flex flex-col items-center justify-center w-full px-2 space-y-5 text-center lg:w-1/2 lg:pe-24 lg:text-left">
                     <h1 className="text-2xl font-bold lg:text-6xl">
                         Post Your Job Vacancy
                     </h1>
                     <p className="text-xs lg:text-sm">
-                        Create job listings for the positions you re offering.
+                        Create job listings for the positions you're offering.
                         Receive daily notifications and directly accept
                         applications from potential candidates. Sign up now for
                         free and start posting your job vacancies.
